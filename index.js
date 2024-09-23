557ac1d257418f4059a4dc69f283b94d68225c16
J-Shyam <shamjanjanam2424@gmail.com>
2024-09-23 17:56:06 +0530

    add

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const router = require('./routes');
 
 const app = express();
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
+    origin : "*",
     credentials : true
 }));
 app.use(express.json());
