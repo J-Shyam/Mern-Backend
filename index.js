6c15731392289c4e6c77edcb2027104efa90b175
J-Shyam <shamjanjanam2424@gmail.com>
2024-09-25 11:38:19 +0530

    frontendurl

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const router = require('./routes');
 
 const app = express();
 app.use(cors({
-    origin : "*",
+    origin : process.env.FRONTEND_URL,
     credentials : true
 }));
 app.use(express.json());
